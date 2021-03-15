f870281447e70805825b05595da178c547ef3c5a
Bryan H <34315804+bryann03@users.noreply.github.com>
2021-03-15 11:09:56 +0100

    add diseño aplicacion

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,6 +1,21 @@
 <template>
     <main>
-        <h1 class="text-center">Color predominante</h1>
+        <h1 class="text-center">Color de imágen</h1>
+
+        <div class="jumbotron">
+            <div class="row">
+                <div class="col-6">
+                    <button type="button" class="btn btn-primary">Subir imágen</button>
+                </div>
+                <div class="col-6 text-center">
+                    <h3>Color predominante</h3>
+                    <div class="card" style="height: 18rem;">
+                        <div class="card-body">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </main>
 </template>
@@ -15,4 +30,7 @@ export default {
 </script>
 
 <style lang="scss">
+.card-body{
+    background-color: aqua;
+}
 </style>
